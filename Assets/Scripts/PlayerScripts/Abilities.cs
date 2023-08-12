f8f752b994683f8cb239220a1e43378478f6eb10
Modesto Amador <mofesdo.dev@gmail.com>
2023-08-12 10:20:08 -0700

    Player WASD Movement Added
    Basic left and right movement added as well as a sprint functionality with Left Shift Key.

---
+++ Assets/Scripts/PlayerScripts/Abilities.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MetroidvaniaTools
+{
+
+    public class Abilities : Character
+    {
+        protected Character character;
+
+        protected override void Initialization()
+        {
+            base.Initialization();
+            character = GetComponent<Character>();
+        }
+    }
+}

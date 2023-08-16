d300551023d36476730d957ae414f94cac44ef57
Modesto Amador <mofesdo.dev@gmail.com>
2023-08-15 20:48:35 -0700

    started implementing weapon system and object pooling

---
+++ Assets/Scripts/WeaponScripts/ObjectPooler.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MetroidvaniaTools
+{
+    public class ObjectPooler : MonoBehaviour
+    {
+        private GameObject currentItem;
+
+         public void createPool(WeaponTypes weapon)
+        {
+            for(int i = 0; i < weapon.amountToPool;  i++)
+            {
+                currentItem = Instantiate(weapon.projectile);
+                currentItem.SetActive(false);
+            }
+        }
+    }
+}

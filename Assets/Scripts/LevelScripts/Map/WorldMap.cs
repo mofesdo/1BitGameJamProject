afa741e0bf88fb5fb3a37ea53c1ecce03275411b
Modesto Amador <mofesdo.dev@gmail.com>
2023-08-18 08:44:12 -0700

    mini map and world map added

---
+++ Assets/Scripts/LevelScripts/Map/WorldMap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MetroidvaniaTools
+{
+    public class WorldMap : Managers
+    {
+        public Bounds bounds;
+
+        protected virtual void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(transform.localPosition + transform.parent.position, bounds.size);
+        }
+    }
+}
